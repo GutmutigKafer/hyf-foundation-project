40e3a8e3d14787c50bfaa05307342ec288194b10
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2026-02-02 09:11:01 +0100

    adding the flippable card

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Creating test
+const flipCard = document.querySelector(".flip-card");
+
+if (flipCard) {
+  flipCard.addEventListener("click", handleFlip);
+}
+
+function handleFlip(event) {
+  const cardInner = event.currentTarget.querySelector(".flip-card-inner");
+
+  if (cardInner) {
+    cardInner.classList.toggle("flipped");
+    //console.log("Card flipped");
+  }
+}
