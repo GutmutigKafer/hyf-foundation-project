1aa9f58e16348d417f400db5a697e9503451829d
GutmutigKafer <vladonozes@gmail.com>
2026-02-03 16:51:45 +0100

    Started working on a middle card / placeholder on uneven grids

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const gridDiv = document.querySelector(".grid");
 
 const cardTypes = [
-  { type: "circle", pic: "url1010" },
-  { type: "triangle", pic: "url2020" },
-  { type: "square", pic: "url3030" },
-  { type: "rectangle", pic: "url4040" },
-  { type: "pentagon", pic: "url5050" },
-  { type: "hexagon", pic: "url6060" },
-  { type: "heptagon", pic: "url7070" },
-  { type: "octagon", pic: "url8080" },
-  { type: "nonagon", pic: "url9090" },
-  { type: "decagon", pic: "url10010" },
-  { type: "star", pic: "url11110" },
-  { type: "heart", pic: "url12120" },
-  { type: "oval", pic: "url13130" },
-  { type: "diamond", pic: "url14140" },
+  {
+    type: "circle",
+    pic: "https://img.icons8.com/sf-black-filled/100/filled-circle.png",
+  },
+  {
+    type: "triangle",
+    pic: "https://img.icons8.com/glyph-neue/100/sort-down.png",
+  },
+  {
+    type: "square",
+    pic: "https://img.icons8.com/material-sharp/100/unchecked-checkbox.png",
+  },
+  { type: "rectangle", pic: "https://img.icons8.com/android/96/rectangle.png" },
+  {
+    type: "pentagon",
+    pic: "https://img.icons8.com/forma-light-filled-sharp/96/quintile-72.png",
+  },
+  { type: "hexagon", pic: "https://img.icons8.com/officel/80/hexagon.png" },
+  {
+    type: "heptagon",
+    pic: "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/96/external-glyph-shapes-tanah-basah-glyph-tanah-basah-25.png",
+  },
+  { type: "octagon", pic: "https://img.icons8.com/fluency/96/octagon.png" },
+  { type: "star", pic: "https://img.icons8.com/glyph-neue/64/filled-star.png" },
+  { type: "heart", pic: "https://img.icons8.com/glyph-neue/64/like--v1.png" },
+  { type: "oval", pic: "https://img.icons8.com/android/96/ellipse.png" },
+  { type: "diamond", pic: "https://img.icons8.com/poly/90/diamond.png" },
 ];
 
 let gridSize = 16;
@@ -24,21 +37,30 @@ const createCards = (gridSize, types) => {
 
   // Checking how many "types" we need depending on the grid size, then combining two array copies to create doubles
   const needTypes = Math.floor(gridSize / 2);
-  const useTypes = types.slice(0, needTypes + 1);
+  const useTypes = types.slice(0, needTypes);
+  console.log(useTypes);
   const grid = new Array(gridSize);
   const allTypes = useTypes.concat(useTypes);
+
   for (let i = 0; i < grid.length; i++) {
-    const typeIndx = Math.floor(Math.random() * allTypes.length);
-    const cardType = allTypes[typeIndx].type;
-    const cardPicture = allTypes[typeIndx].pic;
-    const card = {
+    let card = {
       // This is a placeholder until we connect the API
       id: i + 1,
-      name: cardType,
-      picture: cardPicture,
-      status: gridSize % 2 !== 0 && i === needTypes ? "placeholder" : "active",
     };
+    const typeIndx = Math.floor(Math.random() * allTypes.length);
+    let cardType;
+    let cardPicture;
 
+    if (gridSize % 2 !== 0 && i === needTypes) {
+      card.status = "placeholder";
+    } else {
+      cardType = allTypes[typeIndx].type;
+      cardPicture = allTypes[typeIndx].pic;
+      card.name = cardType;
+      card.picture = cardPicture;
+      card.status = "active";
+      allTypes.splice(typeIndx, 1);
+    }
     grid[i] = card;
 
     //   <div class="flip-card">
@@ -59,20 +81,20 @@ const createCards = (gridSize, types) => {
     backSide.setAttribute("class", "back-side");
 
     const picture = document.createElement("img");
-    // picture.setAttribute("src", cardPicture);
+    picture.setAttribute("src", cardPicture);
     backSide.appendChild(picture);
 
     flipCardInner.appendChild(frontSide);
     flipCardInner.appendChild(backSide);
 
     gridDiv.appendChild(flipCard);
-    allTypes.splice(typeIndx, 1);
   }
 
   gridDiv.setAttribute(
     "style",
     `grid-template-columns: repeat(${Math.sqrt(gridSize)}, 1fr)`,
   );
+
   return grid;
 };
 
